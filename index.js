f98a1f2573bf6c19e612ac0e2e66b2bf35a98ea5
xiangshouding <fansekey@gmail.com>
2014-06-13 22:12:42 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+model.exports = (function () {
+
+    function warning(s) {
+        console.log('[WARN] ' + s + '\n')
+    }
+
+    function error(s) {
+        console.log('[ERROR] ' + s + '\n');
+    }
+
+    function fatal(s) {
+        console.log('[FATAL] ' + s + '\n');
+    }
+
+    function notice(s) {
+        console.log('[NOTICE] ' + s + '\n');
+    }
+
+    return {
+        warning: warning,
+        error: error,
+        fatal: error,
+        notice: notice
+    }
+})();
